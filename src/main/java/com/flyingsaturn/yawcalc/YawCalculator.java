4402614b3e1e1c3e486babe29c3eca298fc585e1
FlyingSaturn <56539009+FlyingSaturn@users.noreply.github.com>
2024-07-26 02:52:17 +0530

    Update YawCalculator.java

--- src/main/java/com/flyingsaturn/yawcalc/YawCalculator.java
+++ src/main/java/com/flyingsaturn/yawcalc/YawCalculator.java
@@ -34,7 +34,7 @@ public class YawCalculator extends JavaPlugin
 
             try {
                 int x_current = Integer.parseInt(args[0]);
-                int z_current = Integer.parseInt(args[1]);
+                int z_current = Integer.parseInt(args[1]); // z hasn't been negated
                 int x_dest = Integer.parseInt(args[2]);
                 int z_dest = Integer.parseInt(args[3]);
                 String yaw = calculateyaw(x_current, z_current, x_dest, z_dest);
@@ -50,10 +50,10 @@ public class YawCalculator extends JavaPlugin
         return false;
     }
 
-    private String calculateyaw(int xCurrent, int zCurrent, int xDest, int zDest) // Values of z are already negated
+    private String calculateyaw(int xCurrent, int zCurrent, int xDest, int zDest) // Values of z are to be negated
     {
         // atan2 is for finding the angle of a line in radians, which is converted to degrees
-        double s = Math.toDegrees(Math.atan2(zDest - zCurrent, xDest - xCurrent));
+        double s = Math.toDegrees(Math.atan2(zCurrent - zDest, xDest - xCurrent)); // Do you see the negation? z1 - z2 instead of z2 - z1?
         s = (s == 180) ? -180 : s; //-180 is producing the correct value
         double slope = s + 90; // Rotating the whole minecraft yaw table 90 degrees anticlockwise
         slope *= -1; // On rotation, we get negative values in the positive quadrants of 'y' (or -Z axis)
